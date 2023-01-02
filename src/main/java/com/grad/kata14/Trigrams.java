80d6de9446359d337c0a265b61e4b5e3e08274a5
Kristof_Szabo <kristofjozsef.szabo@tesco.com>
2023-01-02 09:21:05 +0100

    kata14 - small update

--- src/main/java/com/grad/kata14/Trigrams.java
+++ src/main/java/com/grad/kata14/Trigrams.java
@@ -16,18 +16,20 @@ public class Trigrams {
 
     public static void main(String[] args) {
         //        String originalSentence = "I wish I may I wish I might";
-        String originalSentence = "Egy kutya setal a parkban. Egy kutya szalad egy bot utan a parkban.";
+        String originalSentence = "Egy kutya setal a parkban. Egy kutya szalad egy bot utan a parkban. Egy kutya szalad egy labda utan a holdon.";
 
         String[] strArr = originalSentence.split(" ");
 
         Map<String, List<String>> hMap = new HashMap<>();
 
         for (int i = 0; i < strArr.length - 2; i++) {
-            String temp = strArr[i] + strArr[i + 1];
+            String temp = strArr[i] + " " + strArr[i + 1];
             List<String> alterWords = new ArrayList<>();
             for (int j = i + 1; j < strArr.length - 1; j++) {
-                if (!hMap.containsKey(temp) && (strArr[j - 1] + strArr[j]).equals(temp)) {
-                    alterWords.add(strArr[j + 1]);
+                if (!hMap.containsKey(temp) && (strArr[j - 1] + " " + strArr[j]).equals(temp)) {
+                    if (!alterWords.contains(strArr[j + 1].toLowerCase())) {
+                        alterWords.add(strArr[j + 1].toLowerCase());
+                    }
                 }
             }
             hMap.putIfAbsent(temp, alterWords);
