f28ea96f19b48e64addae1e4c2172a1d0ee52edb
Kristof_Szabo <kristofjozsef.szabo@tesco.com>
2023-01-14 14:58:10 +0100

    kata11 - update with groupBy

--- src/main/java/com/grad/kata11/SortingCharacters.java
+++ src/main/java/com/grad/kata11/SortingCharacters.java
@@ -1,7 +1,9 @@
 package com.grad.kata11;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class SortingCharacters {
 
@@ -14,7 +16,7 @@ public class SortingCharacters {
 
     public static String decodeString(String originalString) {
         String removedWhitespaces = sanitizeOriginalString(originalString);
-        Map<Character, Integer> ascii = countEachCharacterFromString(removedWhitespaces);
+        Map<Character, List<Character>> ascii = countEachCharacterFromString(removedWhitespaces);
         return sortCountedCharacter(ascii);
     }
 
@@ -22,25 +24,17 @@ public class SortingCharacters {
         return originalString.toLowerCase().replaceAll("[,.\\s]", "");
     }
 
-    private static String sortCountedCharacter(Map<Character, Integer> ascii) {
+    private static String sortCountedCharacter(Map<Character, List<Character>> ascii) {
         StringBuilder builder = new StringBuilder();
-        for (Map.Entry<Character, Integer> c : ascii.entrySet()) {
-            builder.append(String.valueOf(c.getKey()).repeat(c.getValue()));
+        for (Map.Entry<Character, List<Character>> c : ascii.entrySet()) {
+            builder.append(String.valueOf(c.getKey()).repeat(c.getValue().size()));
         }
         return builder.toString();
     }
 
-    private static Map<Character, Integer> countEachCharacterFromString(String sanitizedSentence) {
-        Map<Character, Integer> ascii = new HashMap<>();
-        for (int i = 0; i < sanitizedSentence.length(); i++) {
-            Character character = sanitizedSentence.charAt(i);
-            if (!ascii.containsKey(character)) {
-                ascii.put(character, 1);
-            } else {
-                Integer num = ascii.get(character);
-                ascii.put(character, ++num);
-            }
-        }
-        return ascii;
+    private static Map<Character, List<Character>> countEachCharacterFromString(String sanitizedSentence) {
+        return sanitizedSentence.chars().mapToObj(c -> (char) c)
+                .collect(groupingBy(character -> character));
+
     }
 }
